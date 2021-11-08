b4d447b55374172d05aca6bfed98926a2bea4e07
gideona1 <75781877+gideona1@users.noreply.github.com>
2021-11-07 20:06:58 -0500

    Standard Deviation

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -2,7 +2,7 @@
  * RandomGenerator : main class for comparing each random class
  * 
  * @author Gideon Antwi & Chindeum Egeh
- * @version 10/30/2021
+ * @version 11/7/2021
  */
 
 import java.util.Random;
@@ -15,9 +15,9 @@ public class RandomGenerator {
      * Request the same size/range integers from Random.org using their HTTP Interface. [DONE]
      * Create Eucledian Algorithm. [DONE]
      * Count the number of pairs with gcd = 1 using the Eucledian algorithm [DONE]
-     * Calculate the statistical estimate of Pi using Cesaro's Theorem from the TRNG, PRNG, and CSPRNG. []
-     * Create 30 statistical estimates of Pi []
-     * Generate a frequency distribution table, calculate the average, and the standard deviation for each RNG. []
+     * Calculate the statistical estimate of Pi using Cesaro's Theorem from the TRNG, PRNG, and CSPRNG. [DONE -]
+     * Create 30 statistical estimates of Pi [DONE]
+     * Generate a frequency distribution table, calculate the average, and the standard deviation for each RNG. [DONE]
      */
     
     public static void main(String[] args) {
@@ -50,17 +50,17 @@ public class RandomGenerator {
             System.out.println("(TRNG) GCD Equal to 1: " + GCDCount(trng));
             trngEstimates[i] = GetPIEstimate(GCDCount(trng), 100);
             System.out.println("(TRNG) PI Estimate: " + trngEstimates[i]);
-            System.out.println("(TRNG) Difference: " + (trngEstimates[i] -  Math.PI)  + "\n");
+            System.out.println("(TRNG) Difference From PI: " + (trngEstimates[i] -  Math.PI)  + "\n");
 
             System.out.println("(PRNG) GCD Equal to 1: " + GCDCount(prng));
             prngEstimates[i] = GetPIEstimate(GCDCount(prng), 100);
             System.out.println("(PRNG) PI Estimate: " + prngEstimates[i]);
-            System.out.println("(PRNG) Difference: " + (prngEstimates[i] -  Math.PI)  + "\n");
+            System.out.println("(PRNG) Difference From PI: " + (prngEstimates[i] -  Math.PI)  + "\n");
 
             System.out.println("(CSPRNG) GCD Equal to 1: " + GCDCount(csprng));
             csprngEstimates[i] = GetPIEstimate(GCDCount(csprng), 100);
             System.out.println("(CSPRNG) PI Estimate: " + csprngEstimates[i]);
-            System.out.println("(CSPRNG) Difference: " + (csprngEstimates[i] -  Math.PI)  + "\n");
+            System.out.println("(CSPRNG) Difference From PI: " + (csprngEstimates[i] -  Math.PI)  + "\n");
         }
 
         // Averages
@@ -75,15 +75,31 @@ public class RandomGenerator {
         System.out.println("FREQUENCY TABLE: ");
         System.out.println("-----------------------------");
         GenerateTable(prngEstimates, trngEstimates, csprngEstimates);
+
+        // Standard Deviation
+        System.out.println("STANDARD DEVIATION: ");
+        System.out.println("-----------------------------");
+
+        System.out.println("(TRNG) Standard Deviation: " + GetStandardDev(trngEstimates));
+        System.out.println("(PRNG) Standard Deviation: " + GetStandardDev(prngEstimates));
+        System.out.println("(CSPRNG) Standard Deviation: " + GetStandardDev(csprngEstimates) + "\n");
+
+        // Check for TRNG server errors
+        System.out.println(ErrorCheck(trngEstimates));
     }
 
-    public static double GetAverage(double[] estimates) {
+    /**
+     * Get average (mean) of all values in array
+     * @param array
+     * @return
+     */
+    public static double GetAverage(double[] array) {
         double total = 0;
-        for (int i = 0; i < estimates.length; i++) {
-            total += estimates[i];
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
         }
 
-        return total / estimates.length;
+        return total / array.length;
     }
 
     /**
@@ -155,18 +171,49 @@ public class RandomGenerator {
 
     /**
      * Generate frequency dist. table
-     * @param prngEstimate
-     * @param trngEstimate
-     * @param csprngEstimate
+     * @param prngEstimate PRNG PI estimate array
+     * @param trngEstimate TRNG PI estimate array
+     * @param csprngEstimate CSPRNG PI estimate array
      */
     public static void GenerateTable(double[] prngEstimate, double[] trngEstimate, double[] csprngEstimate) {
         System.out.printf("%10s| %8s| %8s| %8s| %8s%n", "ESTIMATES", "TRNG", "PRNG", "CSPRNG", "OVERALL");
-        System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "< 3", EstimateCount(trngEstimate, 0, 3), EstimateCount(prngEstimate, 0, 3), EstimateCount(csprngEstimate, 0, 3), (EstimateCount(trngEstimate, 0, 3) + EstimateCount(prngEstimate, 0, 3) + EstimateCount(csprngEstimate, 0, 3)));
+        System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "< 3", EstimateCount(trngEstimate, -999, 3), EstimateCount(prngEstimate, -999, 3), EstimateCount(csprngEstimate, -999, 3), (EstimateCount(trngEstimate, -999, 3) + EstimateCount(prngEstimate, -999, 3) + EstimateCount(csprngEstimate, -999, 3)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "3 - 3.2", EstimateCount(trngEstimate, 3, 3.2), EstimateCount(prngEstimate, 3, 3.2), EstimateCount(csprngEstimate, 3, 3.2), (EstimateCount(trngEstimate, 3, 3.2) + EstimateCount(prngEstimate, 3, 3.2) + EstimateCount(csprngEstimate, 3, 3.2)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "3.2 - 3.4", EstimateCount(trngEstimate, 3.2, 3.4), EstimateCount(prngEstimate, 3.2, 3.4), EstimateCount(csprngEstimate, 3.2, 3.4), (EstimateCount(trngEstimate, 3.2, 3.4) + EstimateCount(prngEstimate, 3.2, 3.4) + EstimateCount(csprngEstimate, 3.2, 3.4)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "3.4 - 3.6", EstimateCount(trngEstimate, 3.4, 3.6), EstimateCount(prngEstimate, 3.4, 3.6), EstimateCount(csprngEstimate, 3.4, 3.6), (EstimateCount(trngEstimate, 3.4, 3.6) + EstimateCount(prngEstimate, 3.4, 3.6) + EstimateCount(csprngEstimate, 3.4, 3.6)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "3.6 - 3.8", EstimateCount(trngEstimate, 3.6, 3.8), EstimateCount(prngEstimate, 3.6, 3.8), EstimateCount(csprngEstimate, 3.6, 3.8), (EstimateCount(trngEstimate, 3.6, 3.8) + EstimateCount(prngEstimate, 3.6, 3.8) + EstimateCount(csprngEstimate, 3.6, 3.8)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "3.8 - 4", EstimateCount(trngEstimate, 3.8, 4), EstimateCount(prngEstimate, 3.8, 4), EstimateCount(csprngEstimate, 3.8, 4), (EstimateCount(trngEstimate, 3.8, 4) + EstimateCount(prngEstimate, 3.8, 4) + EstimateCount(csprngEstimate, 3.8, 4)));
         System.out.printf("%10s| %8d| %8d| %8d| %8d%n", "> 4", EstimateCount(trngEstimate, 4, 999), EstimateCount(prngEstimate, 4, 999), EstimateCount(csprngEstimate, 4, 999), (EstimateCount(trngEstimate, 4, 999) + EstimateCount(prngEstimate, 4, 999) + EstimateCount(csprngEstimate, 4, 999)));
+        System.out.println();
+    }
+
+    /**
+     * Get standard deviation from array
+     * @param estimate - array
+     * @return
+     */
+    public static double GetStandardDev(double[] estimate) {
+        // Get Mean of array
+        double mean = GetAverage(estimate);
+
+        // Subtract each value in array by mean and square it, then store result in another array
+        double[] newSet = new double[estimate.length];
+        for (int i = 0; i < estimate.length; i++) {
+            newSet[i] = Math.pow((estimate[i] - mean), 2);
+        }
+
+        double newSetMean = GetAverage(newSet);
+
+        return Math.sqrt(newSetMean);
+    }
+
+    public static String ErrorCheck(double[] trngValue) {
+        for(double values : trngValue) {
+            if(values == 0.0) {
+                return "Done. Results for TRNG may not be accurate due to one or more website connection error.";
+            }
+        }
+
+        return "Done. The program ran with no errors.";
     }
 }
